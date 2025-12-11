bab188e9e0c9e94475d9d4ae56ddfb468aff324c
Nguyễn Ngọc Truyện <nntruyen027@gmail.com>
2025-12-11 16:36:32 +0700

    Chỉnh sửa thông tin của quản trị viên

--- src/main/java/com/vinhthanh2/lophocdientu/controller/admin/AdminController.java
+++ src/main/java/com/vinhthanh2/lophocdientu/controller/admin/AdminController.java
@@ -16,7 +16,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/quan-tri")
 @Getter
 @Setter
